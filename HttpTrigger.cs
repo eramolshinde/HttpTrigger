f9485ed0817d80e14030068b3fb05021e9065895
Amol Shinde <amol.shinde@eviden.com>
2024-02-16 12:45:37 +0530

    Added TraceWriter

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -1,12 +1,11 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace HttpTrigger
 {
@@ -15,15 +14,15 @@ namespace HttpTrigger
         [FunctionName("HttpTrigger")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            TraceWriter log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.Info("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name ??= data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
